1b02a5dfb3dc4f8308d0575a0f06c037d7d9a6e9
MatiasStorm <48760011+MatiasStorm@users.noreply.github.com>
2019-09-19 12:08:57 +0200

    Updates

---
+++ precache-manifest.272a9f0d3066f064383b7accfd687345.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f216d9b0d6a19805018e9c9eb321e3a0",
+    "url": "/Random-News/index.html"
+  },
+  {
+    "revision": "03446240b64eb7daa7c8",
+    "url": "/Random-News/static/css/main.997afbb8.chunk.css"
+  },
+  {
+    "revision": "43cdd167fdd14ea827dd",
+    "url": "/Random-News/static/js/2.0dc1ea6e.chunk.js"
+  },
+  {
+    "revision": "03446240b64eb7daa7c8",
+    "url": "/Random-News/static/js/main.0d05f5a0.chunk.js"
+  },
+  {
+    "revision": "8b2ae11f76e071939181",
+    "url": "/Random-News/static/js/runtime~main.580dcf8a.js"
+  }
+]);
