d59eb00fe1e69f85275ec68505c5b9e7031372a5
MatiasStorm <48760011+MatiasStorm@users.noreply.github.com>
2019-09-19 15:53:48 +0200

    Updates

---
+++ precache-manifest.302acc990c15c8b1c88a485c00deaa44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd37a67f201e29966f943f676ad8213e",
+    "url": "/Random-News/index.html"
+  },
+  {
+    "revision": "4f3948e90c9ec56a1893",
+    "url": "/Random-News/static/css/main.ca64c909.chunk.css"
+  },
+  {
+    "revision": "02f7745907bcea0b2075",
+    "url": "/Random-News/static/js/2.58f3204c.chunk.js"
+  },
+  {
+    "revision": "4f3948e90c9ec56a1893",
+    "url": "/Random-News/static/js/main.3b002df1.chunk.js"
+  },
+  {
+    "revision": "8b2ae11f76e071939181",
+    "url": "/Random-News/static/js/runtime~main.580dcf8a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Random-News/precache-manifest.1aa63d888c75608bd2488776ba0dc1c2.js"
+  "/Random-News/precache-manifest.302acc990c15c8b1c88a485c00deaa44.js"
 );
 
 self.addEventListener('message', (event) => {
